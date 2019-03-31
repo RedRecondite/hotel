d8e97ae735bdb6cbf66c153fee89d4138cc3f8e1
RedRecondite <dwalma@gmail.com>
2019-03-31 14:16:10 -0400

    Skurn: Added s0-dude.c: he's the chosen one

---
+++ story/s0-dude.c
@@ -0,0 +1,10 @@
+void talk
+{
+freeze(1)
+say_stop("`%Hello, I am the dude who will be here taking up space until we", &current_sprite);
+wait(250)
+say_stop("`%can decide on what the store and gameplay is going to be!", &current_sprite);
+wait(250)
+say_stop("Wow, I sure hope you stop loitering in my hotel soon. I already hate you.", 1);
+unfreeze(1)
+}
