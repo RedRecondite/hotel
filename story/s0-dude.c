92d729c05510c201d9bf68920e006d3d7875fdfc
RedRecondite <dwalma@gmail.com>
2019-03-31 14:19:48 -0400

    Skurn: Modified s0-dude.c: i made a booboo

--- story/s0-dude.c
+++ story/s0-dude.c
@@ -3,7 +3,7 @@ void talk
 freeze(1)
 say_stop("`%Hello, I am the dude who will be here taking up space until we", &current_sprite);
 wait(250)
-say_stop("`%can decide on what the store and gameplay is going to be!", &current_sprite);
+say_stop("`%can decide on what the story and gameplay is going to be!", &current_sprite);
 wait(250)
 say_stop("Wow, I sure hope you stop loitering in my hotel soon. I already hate you.", 1);
 unfreeze(1)
