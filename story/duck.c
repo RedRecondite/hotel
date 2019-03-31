ef5ad53b83b8acb21be5dc815db913e1c57f3c1a
RedRecondite <dwalma@gmail.com>
2019-03-31 06:28:17 -0400

    scratcher: Modified duck.c: hitpoints and base walk made duck killable

--- story/duck.c
+++ story/duck.c
@@ -2,4 +2,12 @@ void main( void )
 {
 sp_brain(&current_sprite, 3);
 sp_speed(&current_sprite, 1);
+sp_hitpoints(&current_sprite,1);
+sp_base_walk(&current_sprite,20);
+}
+void die()
+{
+int &ddie = sp_editor_num(&current_sprite);
+if (&ddie != 0)
+editor_type(&ddie,8);
 }
