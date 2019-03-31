8b0b5af71831f8ccf33f81762dc7a9fadbd8e766
RedRecondite <dwalma@gmail.com>
2019-03-31 14:57:34 -0400

    Skurn: Added npc-con1.c:

---
+++ story/npc-con1.c
@@ -0,0 +1,16 @@
+//this is a smallwoodcon-goer
+
+void talk
+{
+freeze(1)
+say_stop("`3Wow, nice cosplay, dude.", &current_sprite);
+wait(250)
+say_stop("Huh?", 1);
+wait(250)
+say_stop("`3I said, it's a nice cosplay. You look just like him.", &current_sprite);
+wait(250)
+say_stop("`3Anyways, I gotta pretend to go to the vendor hall but not actually move.", &current_sprite);
+wait(250)
+say_stop("`3See ya, 'Dink'.", &current_sprite);
+unfreeze(1)
+}
