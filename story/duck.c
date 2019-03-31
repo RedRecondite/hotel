86034a69ae4849f7d448e76074dcc814b8114bf4
RedRecondite <dwalma@gmail.com>
2019-03-31 01:17:32 -0400

    redink1: Modified duck.c: Made it a real duck

--- story/duck.c
+++ story/duck.c
@@ -1,12 +1,5 @@
 void main( void )
 {
-
-set_callback_random( "duck",3000,8000);
-
-}
-
-void duck( void)
-{
- say("I'm a bottle, not a duck.  Even though I sound like one.  Funny, eh?", &current_sprite);
-
+sp_brain(&current_sprite, 3);
+sp_speed(&current_sprite, 1);
 }
