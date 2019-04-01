16ad380138a962b6969015a92b96309e4efae911
RedRecondite <dwalma@gmail.com>
2019-04-01 06:15:22 -0400

    RangerLord: Added bottles-2.c: A script for another bottle shelf.

---
+++ story/bottles-2.c
@@ -0,0 +1,14 @@
+void main(void)
+{
+}
+
+void talk(void)
+{
+ say("Vodka, whiskey, brandy, beer, wine, cognac, rum, tequila, cider, ale, mead - there's booze for everyone.", 1);
+}
+
+void hit(void)
+{
+ say("Stop being so mixed-up!", 1);
+}
+
