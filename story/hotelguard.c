bc98d9f076076682b8498807959e6059d261629f
RedRecondite <dwalma@gmail.com>
2019-04-01 17:55:46 -0400

    Skurn: Modified hotelguard.c: modified to be a simple intro. probably gotta change things a few million times until it works

--- story/hotelguard.c
+++ story/hotelguard.c
@@ -1,5 +1,12 @@
-void talk(void)
+//he is intro boi
+void main
 {
+ if (&story == 1)
+ {
+ goto end;
+ }
+
+ move_stop(1, 8, 231)
  freeze(1);
  freeze(&custom_sprite);
  say_stop("What are you doing here?", 1);
@@ -9,6 +16,14 @@ void talk(void)
  say_stop("I see. What is this hotel called?", 1);
  wait(200);
  say_stop("`1The Dink Hotel. The best inn around.", &current_sprite);
+ wait(200);
+ say_stop("We'll see about that!", 1);
+ status bar
+ &update_status = 1;
+ draw_status();
  unfreeze(1);
  unfreeze(&custom_sprite);
+ &story += 1;
+
+ end:
 }
