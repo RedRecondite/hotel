5023810eb515a5aa2c8ad72dd5282f94a931b0a2
RedRecondite <dwalma@gmail.com>
2019-04-01 19:56:32 -0400

    Skurn: Added outsidemid.c:

---
+++ story/outsidemid.c
@@ -0,0 +1,29 @@
+//random outside music!
+void main
+{
+	int &rand = random(5, 1)
+	if (&rand == 1)
+	{
+		playmidi("1.mid")
+	}
+
+	if (&rand == 2)
+	{
+		 playmidi("11.mid")
+	}
+
+	if (&rand == 3)
+	{
+		 playmidi("16.mid")
+	}
+
+	if (&rand == 4)
+	{
+ 		playmidi("100.mid")
+	}
+
+	if (&rand == 5)
+	{
+		 playmidi("ranmar5.mid")
+	}
+}
