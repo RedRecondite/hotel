0c86e4a7556b6be9abd4e42e0ba90de42bb5a693
RedRecondite <dwalma@gmail.com>
2019-04-01 06:25:16 -0400

    Robj: Added s0-hcount.c: Script for counter in hotel reception area.

---
+++ story/s0-hcount.c
@@ -0,0 +1,9 @@
+void main(void)
+{
+ int &x = sp_x(&current_sprite, -1);
+ int &y = sp_y(&current_sprite, -1);
+ int &crap;
+ 
+ &y = sp_y(&current_sprite, -1);
+ &crap = create_sprite(&x, &y, 0, 381, 1);
+}
