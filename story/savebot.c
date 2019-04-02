cc6fd99f88dbff1d60b38ac85488e6ec7de4df59
RedRecondite <dwalma@gmail.com>
2019-04-02 00:44:37 -0400

    Thenewguy: Added savebot.c:

---
+++ story/savebot.c
@@ -0,0 +1,51 @@
+void main( void )
+{
+	sp_seq(&current_sprite, 449);
+	sp_sound(&current_sprite, 34);
+	sp_brain(&current_sprite, 6);
+	sp_hitpoints(&current_sprite, 0);
+}
+
+void hit( void )
+{
+	say("`3Hit me baby, one more time!", &current_sprite);
+}
+
+void talk( void )
+{
+	Playsound(18,22050,0,0,0);
+		
+	freeze(1);
+	choice_start();
+	"Save your game"
+	"Leave the strange machine"
+	choice_end();
+	unfreeze(1);
+		
+	if (&result == 2)
+	}
+		unfreeze(1);
+		return;
+	}
+	choice_start();
+	"&savegameinfo"
+	"&savegameinfo"
+	"&savegameinfo"
+	"&savegameinfo"
+	"&savegameinfo"
+	"&savegameinfo"
+	"&savegameinfo"
+	"&savegameinfo"
+	"&savegameinfo"
+	"&savegameinfo"
+	"Nevermind"
+	choice_end();
+		
+	unfreeze(1);
+		
+	if (&result  11)
+	{
+		save_game(&result);
+		say_xy("`%Game saved", 1, 30);
+	}
+}
