a35fc8b30e69808afa56f2e5beeb29178f30f2eb
RedRecondite <dwalma@gmail.com>
2019-04-01 20:08:38 -0400

    redink1: Added con-stg.c: Dink's obsession

---
+++ story/con-stg.c
@@ -0,0 +1,13 @@
+void talk(void)
+{
+say("Wow, this is so realistic!", 1);
+}
+
+void hit(void)
+{
+freeze(1);
+say_stop("Soft like a pretty stuffed pony ...", 1);
+say_stop("... that I could hug at night to fall asleep...", 1);
+say_stop("... to remind me of Aunt Maria ...", 1);
+unfreeze(1);
+}
