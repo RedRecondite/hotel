5bab272c88b3a7e18a0fa2ce4e84485a04da4595
RedRecondite <dwalma@gmail.com>
2019-04-01 21:43:26 -0400

    redink1: Modified farmgirl.c: Added farm girl text

--- story/farmgirl.c
+++ story/farmgirl.c
@@ -4,3 +4,13 @@ sp_speed(&current_sprite, 1);
 sp_brain(&current_sprite, 16);
 sp_base_walk(&current_sprite, 330);
 }
+
+void talk(void)
+{
+say("`#Stay away, dude.", &current_sprite);
+}
+
+void hit(void)
+{
+say("`#Stop hitting on me!", &current_sprite);
+}
