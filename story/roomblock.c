6b66b964a82c3688b66b0fcb4426dfb9877beb04
RedRecondite <dwalma@gmail.com>
2019-04-02 06:16:53 -0400

    redink1: Added roomblock.c:

---
+++ story/roomblock.c
@@ -0,0 +1,21 @@
+void main(void)
+{
+sp_nodraw(&current_sprite, 1);
+sp_hard(&current_sprite, 1);
+draw_hard_sprite(&current)_sprite);
+if (&story < 2)
+{
+sp_touch_damage(&current_sprite, -1);
+}
+}
+
+void touch(void)
+{
+freeze(1);
+sp_touch(damage(&current_sprite, 0);
+move(1, 4, 368, 0);
+say("It's locked!", 1);
+unfreeze(1);
+wait(500);
+sp_touch(damage(&current_sprite, -1);
+}
