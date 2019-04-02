b644e9c4da8d690c72f7a6ff7a418ca89adae590
RedRecondite <dwalma@gmail.com>
2019-04-02 01:37:23 -0400

    Thenewguy: Modified bed-2.c: fixes

--- story/bed-2.c
+++ story/bed-2.c
@@ -1,5 +1,6 @@
 void main(void)
 {
+int &num = random(2, 0);
 }
 
 void talk(void)
@@ -14,32 +15,38 @@ choice_start();
 choice_end();
 if (&result == 1)
 {
-say_stop("I guess I could use some sleep...", 1);
+say_stop("I suppose I COULD use some sleep...", 1);
 wait(100);
 fade_down();
 wait(1500);
-int &random = random(1, 0);
-if (&random == 0)
+&num += 1;
+if (&num >= 2)
 {
-say_xy("`7Hello, Dink.", 1, 30);
-wait(2000);
-say_xy("`7It's me, the man of your dreams!", 2, 30);
-wait(3000);
-say_xy("`7Don't tell anyone I spoke to you!", 3, 30);
-wait(4000);
-say_xy("`7It'll be...our little secret...", 4, 30);
-wait(4000);
+&num = 0;
 }
-if (&random == 1)
+if (&num == 0)
 {
-say_xy("`4You are a pony.", 1, 30);
-wait(3000);
-say_xy("`4Not a horse!", 2, 30);
-wait(3000);
+say_stop_xy("`7Hello, Dink.", 1, 30);
+wait(500);
+say_stop_xy("`7It's me, the man of your dreams!", 10, 30);
+wait(600);
+say_stop_xy("`7Don't tell anyone I spoke to you!", 20, 30);
+wait(600);
+say_stop_xy("`7It'll be...our little secret...", 30, 30);
+wait(200);
+}
+if (&num == 1)
+{
+say_stop_xy("`4You are a pony.", 1, 30);
+wait(300);
+say_stop_xy("`4Not a horse!", 10, 30);
+wait(300);
+say_stop_xy("`4And don't let anyone ever tell you otherwise...", 20, 30);
+wait(200);
 }
 fade_up();
 wait(200);
-say_stop("This bed makes me have some screwed up dreams...", 1);
+say_stop("Jeez, this bed makes me have some screwed up dreams...", 1);
 wait(100);
 }
 if (&result == 2)
