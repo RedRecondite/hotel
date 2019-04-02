f809dcd4c6a96e8e37c682d68d35751f57eabe3d
RedRecondite <dwalma@gmail.com>
2019-04-01 21:56:42 -0400

    Sparrowhawk: Added spray7.c:

---
+++ story/spray7.c
@@ -0,0 +1,15 @@
+//To move the sprite a random direction/speed in dir7 (up and left)
+//Don't ask what this is used for.
+
+void main
+{
+	int &randx = random(-4, -1);
+	int &randy = random(-4, -2);
+	
+	sp_move_x(&current_sprite, &randx);
+	sp_move_y(&current_sprite, &randy);
+	&randy = random(750, 75);
+	wait(&randy);
+	sp_move_x(&current_sprite, 0);
+	sp_move_y(&current_sprite, 0);
+}
