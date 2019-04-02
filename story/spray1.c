27f0666051d95cb8b5ec810c7b66f159e9231423
RedRecondite <dwalma@gmail.com>
2019-04-01 21:55:03 -0400

    Sparrowhawk: Modified spray1.c: Fixed

--- story/spray1.c
+++ story/spray1.c
@@ -1,13 +1,14 @@
-//To move the sprite a random direction/speed in dir1 (up and right)
+//To move the sprite a random direction/speed in dir1 (down and left)
+//Don't ask what this is used for.
 
 void main
 {
-	int &randx = random(4, 1);
-	int &randy = random(-4, -1);
-	//sp_timing(&current_sprite, 33);
+	int &randx = random(-4, -1);
+	int &randy = random(4, 2);
+
 	sp_move_x(&current_sprite, &randx);
 	sp_move_y(&current_sprite, &randy);
-	&randy = random(1000, 100);
+	&randy = random(750, 75);
 	wait(&randy);
 	sp_move_x(&current_sprite, 0);
 	sp_move_y(&current_sprite, 0);
