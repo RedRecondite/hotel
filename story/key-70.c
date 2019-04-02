7fe5161a45e453409205ae2693f2160b4fa17558
RedRecondite <dwalma@gmail.com>
2019-04-02 07:32:54 -0400

    ExDeathEvn: Modified key-70.c: See comment

--- story/key-70.c
+++ story/key-70.c
@@ -1,4 +1,5 @@
-void main(void)
-{
-  say_stop("This is Screen &player_map and &story story", 1);
-}
+//void main(void)
+//{
+//  say_stop("This is Screen &player_map and &story story", 1);
+//}
+//Commented everything out due to lack of a way to delete this script directly
