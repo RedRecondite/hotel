8cd8e438fc653e91383c9ec68d386b7226ae6fb4
RedRecondite <dwalma@gmail.com>
2019-04-02 01:22:09 -0400

    Quiztis: Modified key-67.c:

--- story/key-67.c
+++ story/key-67.c
@@ -1,20 +1,18 @@
 VOID MAIN ( VOID )
 {
-	lol:
-	script_attach(1000);
+freeze(1);
 	wait(2000);
-	say("Never gonna give you up" , 1);
+	say_stop("Never gonna give you up" , 1);
 	wait(2000);
-		say("Never gonna let you down " , 1);
+		say_stop("Never gonna let you down " , 1);
 		wait(2000);
-			say("Never gonna run around and desert you " , 1);
+			say_stop("Never gonna run around and desert you " , 1);
 			wait(2000);
-				say("Never gonna make you cry" , 1);
+				say_stop("Never gonna make you cry" , 1);
 				wait(2000);
-					say("Never gonna say goodbye" , 1);
+					say_stop("Never gonna say goodbye" , 1);
 					wait(2000);
-						say("Never gonna tell a lie and hurt you" , 1);
-						script_attach(1000);
-						goto lol;
+						say_stop("Never gonna tell a lie and hurt you" , 1);
+						unfreeze(1);
 						
 }
