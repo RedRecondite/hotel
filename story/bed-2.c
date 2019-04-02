6c9d257faa13b4e01094baffb9b4ab4ed897a620
RedRecondite <dwalma@gmail.com>
2019-04-02 03:44:56 -0400

    Thenewguy: Modified bed-2.c: added maxdreams and comments

--- story/bed-2.c
+++ story/bed-2.c
@@ -1,6 +1,8 @@
 void main(void)
 {
-int &num = random(2, 0);
+//if you add a dream, raise maxdreams by 1 so it will cycle properly
+int &maxdreams = 2;
+int &num = random(&maxdreams, 0);
 }
 
 void talk(void)
@@ -13,6 +15,8 @@ choice_start();
 "Wet the bed"
 "I'm not tired"
 choice_end();
+
+//1st choice (Dreams)
 if (&result == 1)
 {
 say_stop("I suppose I COULD use some sleep...", 1);
@@ -20,10 +24,12 @@ wait(100);
 fade_down();
 wait(1500);
 &num += 1;
-if (&num >= 2)
+if (&num >= &maxdreams)
 {
 &num = 0;
 }
+
+//Dream 1
 if (&num == 0)
 {
 say_stop_xy("`7Hello, Dink.", 1, 30);
@@ -35,6 +41,8 @@ wait(600);
 say_stop_xy("`7It'll be...our little secret...", 30, 30);
 wait(200);
 }
+
+//Dream 2
 if (&num == 1)
 {
 say_stop_xy("`4You are a pony.", 1, 30);
@@ -49,6 +57,17 @@ wait(200);
 say_stop("Jeez, this bed makes me have some screwed up dreams...", 1);
 wait(100);
 }
+
+//Dream 3 (placeholder, add your dream here)
+if (&num == 2)
+{
+
+}
+
+//etc...
+
+
+//2nd choice (unfunny joke)
 if (&result == 2)
 {
 say_stop("No! I'm not gonna do that.", 1);
@@ -69,5 +88,5 @@ unfreeze(1);
 
 void hit( void )
 {
-say("It's not truly a Dink Hotel until I've punched everything in here.", 1);
+say("It's not truly a Dink Hotel until I've hit everything in here.", 1);
 }
