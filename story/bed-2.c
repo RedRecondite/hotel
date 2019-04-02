935c18c5b2a2b8984c873169d74c0aa4542740ac
RedRecondite <dwalma@gmail.com>
2019-04-02 01:29:53 -0400

    Thenewguy: Modified bed-2.c: fixes

--- story/bed-2.c
+++ story/bed-2.c
@@ -17,24 +17,25 @@ if (&result == 1)
 say_stop("I guess I could use some sleep...", 1);
 wait(100);
 fade_down();
-wait(1000);
+wait(1500);
 int &random = random(1, 0);
 if (&random == 0)
 {
 say_xy("`7Hello, Dink.", 1, 30);
-wait(400);
-say_xy("`7It's me, the man of your dreams!", 1, 30);
-wait(600);
-say_xy("`7Don't tell anyone I spoke to you!", 1, 30);
-wait(800);
-say_xy("`7It'll be...our little secret...", 1, 30);
+wait(2000);
+say_xy("`7It's me, the man of your dreams!", 2, 30);
+wait(3000);
+say_xy("`7Don't tell anyone I spoke to you!", 3, 30);
+wait(4000);
+say_xy("`7It'll be...our little secret...", 4, 30);
+wait(4000);
 }
 if (&random == 1)
 {
 say_xy("`4You are a pony.", 1, 30);
-wait(300);
-say_xy("`4Not a horse!", 1, 30);
-wait(500);
+wait(3000);
+say_xy("`4Not a horse!", 2, 30);
+wait(3000);
 }
 fade_up();
 wait(200);
@@ -44,13 +45,13 @@ wait(100);
 if (&result == 2)
 {
 say_stop("No! I'm not gonna do that.", 1);
-wait(300);
-say_stop("....". 1);
 wait(100);
+say_stop("....". 1);
+wait(400);
 say_stop("That joke downstairs WAS pretty funny though.", 1);
-wait(300);
-say_stop("....". 1);
 wait(100);
+say_stop("....". 1);
+wait(1000);
 say_stop("Alright fine, but it's just gonna be a tinkle!", 1);
 wait(100);
 }
