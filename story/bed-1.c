0d3e5987f6c6fe00a337e3616c2b253d25de60f9
RedRecondite <dwalma@gmail.com>
2019-04-02 01:08:03 -0400

    Thenewguy: Modified bed-1.c: Added more dialogue.

--- story/bed-1.c
+++ story/bed-1.c
@@ -1,11 +1,14 @@
-
 void main(void)
 {
 }
 
 void talk(void)
 {
- say("Why do all beds I see have the same yellow color?", 1);
+freeze(1);
+say_stop("I wonder why all beds I see have the same yellow color?", 1);
+wait(500);
+say_stop("On second thought, I don't think I want to know.", 1);
+unfreeze(1);
 }
 
 void hit(void)
