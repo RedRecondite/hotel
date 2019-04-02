e862bc2969007b69ea887abfdacad1b19e76a71b
RedRecondite <dwalma@gmail.com>
2019-04-02 01:01:04 -0400

    Thenewguy: Modified fount.c: Added another line for interest

--- story/fount.c
+++ story/fount.c
@@ -2,9 +2,11 @@ void talk( void )
 {
 say("Ah, water. Pretty sure I couldn't live without this stuff.", 1);
 &life += 1;
-if (&life > &lifemax)
+if (&life >= &lifemax)
 {
 &life = &lifemax;
+wait(700);
+say("`4Ah, humans! Pretty sure I could live without them consuming me.", &current_sprite);
 }
 Playsound(10,22050,0,0,0);
 }
